eb470510a48038fb0d3ff7e77fa01a440f4724ed
amatvieieva <anna.matvieieva.work@gmail.com>
2024-07-02 09:53:08 +0300

    Updated button styling in the sidebar

---
+++ src/components/IconComponents/LeftDoubleArrow.js
@@ -0,0 +1,8 @@
+export default function LeftDoubleArrow({className}) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className={className} fill='currentColor'>
+      <path d="M6.4 16 19.463 2.7a1 1 0 0 0-1.426-1.4l-13.75 14a1 1 0 0 0 0 1.4l13.75 14a1 1 0 0 0 1.426-1.4Z" />
+      <path d="M14.652 16 27.713 2.7a1 1 0 0 0-1.426-1.4l-13.75 14a1 1 0 0 0 0 1.4l13.75 14a1 1 0 0 0 1.426-1.4Z" />
+    </svg>
+  );
+}
