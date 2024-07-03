6f35927c0ca72b0535e53977c8952413df2f50c0
amatvieieva <anna.matvieieva.work@gmail.com>
2024-07-03 15:25:14 +0300

    Fixed content width and added animation

--- src/theme/DocRoot/Layout/Sidebar/ExpandButton/index.js
+++ src/theme/DocRoot/Layout/Sidebar/ExpandButton/index.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import {translate} from '@docusaurus/Translate';
-import styles from './styles.module.css';
-import RightDoubleArrow from '@site/src/components/IconComponents/RightDoubleArrow';
+import React from "react";
+import { translate } from "@docusaurus/Translate";
+import styles from "./styles.module.css";
+import RightDoubleArrow from "@site/src/components/IconComponents/RightDoubleArrow";
 
-export default function DocRootLayoutSidebarExpandButton({toggleSidebar}) {
+export default function DocRootLayoutSidebarExpandButton({ toggleSidebar }) {
   return (
     <div
       className={styles.expandButton}
       title={translate({
-        id: 'theme.docs.sidebar.expandButtonTitle',
-        message: 'Expand sidebar',
+        id: "theme.docs.sidebar.expandButtonTitle",
+        message: "Expand sidebar",
         description:
-          'The ARIA label and title attribute for expand button of doc sidebar',
+          "The ARIA label and title attribute for expand button of doc sidebar",
       })}
       aria-label={translate({
-        id: 'theme.docs.sidebar.expandButtonAriaLabel',
-        message: 'Expand sidebar',
+        id: "theme.docs.sidebar.expandButtonAriaLabel",
+        message: "Expand sidebar",
         description:
-          'The ARIA label and title attribute for expand button of doc sidebar',
+          "The ARIA label and title attribute for expand button of doc sidebar",
       })}
       tabIndex={0}
       role="button"
       onKeyDown={toggleSidebar}
-      onClick={toggleSidebar}>
+      onClick={toggleSidebar}
+    >
       <div className={styles.rightArrow__styleBefore}></div>
-      <RightDoubleArrow className='sidebarButtonIcon'/>
+      <RightDoubleArrow className="sidebarButtonIcon" />
       <div className={styles.rightArrow__styleAfter}></div>
     </div>
   );
